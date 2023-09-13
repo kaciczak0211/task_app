2e61688f790990dff9e4e7a5cc1e30fa98522dba
kaciczak0211 <kaciczak0211@gmail.com>
2023-09-13 20:29:43 +0100

    3_btn

--- today.js
+++ today.js
@@ -3,13 +3,22 @@
 let submitBtn = document.getElementById("submitBtn");
 //task list ul
 let allTasks = document.getElementById("all-tasks");
-submitBtn.addEventListener("click", function(){
+
+let counter = 0;
+function addTask(){
     let li = document.createElement("li");
-    let closeBtn = document.createElement("div")
-    closeBtn.innerHTML = `<p>Edit</p><p>Delete</p>
-    `
-    closeBtn.style.display = "flex"
-    closeBtn.style.gap = "2rem";
+    li.classList = "task-style"
+    li.id = 'taks ' + counter++;
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete"
+    deleteBtn.classList = "delete"
+    deleteBtn.style.display = "flex"
+    deleteBtn.style.gap = "2rem";
+    let editBtn = document.createElement("button");
+    editBtn.textContent = "edit"
+    editBtn.classList = "edit"
+    editBtn.style.display = "flex"
+    editBtn.style.gap = "2rem";
     let filedInput = document.getElementById("filed").value;
     let taskName = document.createTextNode(filedInput);
     li.appendChild(taskName);
@@ -18,12 +27,37 @@ submitBtn.addEventListener("click", function(){
         return
     }else{
         document.getElementById("all-tasks").appendChild(li)
-        li.appendChild(closeBtn);
+        li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
         // li.appendChild(editBtn);
     }
+
     //empty input after click
     document.getElementById("filed").value = "";
-})
+    deleteBtn.addEventListener("click", function(){
+        li.parentNode.removeChild(li);
+    })
+    editBtn.addEventListener("click", function(){
+        allTasks.contentEditable = "true";
+        li.style.backgroundColor = "black";
+        let doneBtn = document.createElement("button");
+        doneBtn.textContent = "done"
+        doneBtn.classList = "done"
+        doneBtn.style.display = "flex"
+        doneBtn.style.gap = "2rem";
+        li.appendChild(doneBtn);
+        doneBtn.addEventListener("click", function(){
+            doneBtn.remove();
+            li.style.backgroundColor = "#71c7ec";
+        })
+    })
+
+
+}
+
+
+
+
 
 
 
